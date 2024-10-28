a50741901b38fddfb3c502e52bbf7cb0a6315b5d
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-28 11:40:12 +0300

    add new file conftest.py

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,43 +1,25 @@
 import pytest
-from unittest.mock import Mock
 from praktikum.burger import Burger
-from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
 
 class TestBurger:
-    @pytest.fixture  # Mок для класса Bun, который возвращает имя и цену булочки
-    def bun_mock(self):
-        bun = Mock(spec=Bun)
-        bun.get_name.return_value = "Булочка"
-        bun.get_price.return_value = 50.0
-        return bun
-
-    @pytest.fixture  # Mок для класса Ingredient, который возвращает имя, цену и тип ингредиента
-    def ingredient_mock(self):
-        ingredient = Mock(spec=Ingredient)
-        ingredient.get_name.return_value = "помидор"
-        ingredient.get_price.return_value = 10.0
-        ingredient.get_type.return_value = "начинка"
-        return ingredient
-
     def test_set_buns(self, bun_mock):  # Проверяем, что метод set_buns устанавливает булочку
         burger = Burger()
         burger.set_buns(bun_mock)
         assert burger.bun == bun_mock
 
-    def test_add_ingredient(self, ingredient_mock):  # Проверяем, что метод добавляет ингредиен
+    def test_add_ingredient(self, ingredient_mock):  # Проверяем, что метод добавляет ингредиент
         burger = Burger()
         burger.add_ingredient(ingredient_mock)
         assert len(burger.ingredients) == 1
         assert burger.ingredients[0] == ingredient_mock
 
-    def test_remove_ingredient(self, ingredient_mock): # Проверяем, что удаляет ингредиент
+    def test_remove_ingredient(self, ingredient_mock):  # Проверяем, что удаляет ингредиент
         burger = Burger()
         burger.add_ingredient(ingredient_mock)
         burger.remove_ingredient(0)
         assert len(burger.ingredients) == 0
 
-    def test_move_ingredient(self, ingredient_mock): # Проверяем, что метод перемещает ингредиент
+    def test_move_ingredient(self, ingredient_mock):  # Проверяем, что метод перемещает ингредиент
         burger = Burger()
         burger.add_ingredient(ingredient_mock)
         burger.add_ingredient(ingredient_mock)  # Добавляем еще один ингредиент
@@ -45,13 +27,13 @@ class TestBurger:
         assert burger.ingredients[1] == ingredient_mock
         assert burger.ingredients[0] == ingredient_mock
 
-    def test_get_price(self, bun_mock, ingredient_mock): # Проверяем, что метод возвращает правильную цену
+    def test_get_price(self, bun_mock, ingredient_mock):  # Проверяем, что метод возвращает правильную цену
         burger = Burger()
         burger.set_buns(bun_mock)
         burger.add_ingredient(ingredient_mock)
         assert burger.get_price() == 110.0  # 50 (булочка) * 2 + 10 (ингредиент)
 
-    def test_get_receipt(self, bun_mock, ingredient_mock): # Проверяем, что метод возвращает правильный чек
+    def test_get_receipt(self, bun_mock, ingredient_mock):  # Проверяем, что метод возвращает правильный чек
         burger = Burger()
         burger.set_buns(bun_mock)
         burger.add_ingredient(ingredient_mock)
